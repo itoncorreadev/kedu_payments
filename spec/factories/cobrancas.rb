fb26d3cf5b47c8b8e4c3b0dd16fc7bd1679d1c26
Cleiton Corrêa <cleitoncorreas@gmail.com>
2025-11-17 07:32:10 -0300

    test(factories): ajusta factorie cobrancas.rb

--- spec/factories/cobrancas.rb
+++ spec/factories/cobrancas.rb
@@ -1,9 +1,29 @@
 FactoryBot.define do
   factory :cobranca do
     association :plano_de_pagamento
-    valor_cents { rand(50..100) * 100 }
-    data_vencimento { Date.current + rand(1..60) }
-    metodo_pagamento { %i[boleto pix].sample }
+    valor_cents { 100_00 }
+    data_vencimento { Date.current + 30 }
+    metodo_pagamento { :boleto }
     status { :emitida }
+
+    trait :pix do
+      metodo_pagamento { :pix }
+    end
+
+    trait :paga do
+      status { :paga }
+    end
+
+    trait :cancelada do
+      status { :cancelada }
+    end
+
+    trait :vencida do
+      data_vencimento { Date.yesterday }
+    end
+
+    trait :vincenda do
+      data_vencimento { Date.tomorrow }
+    end
   end
 end
