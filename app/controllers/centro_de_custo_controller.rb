530e0606b2c39b48a53418029bf93829bd892b94
Cleiton Corrêa <cleitoncorreas@gmail.com>
2025-11-17 05:47:06 -0300

    feat(controller): adiciona controller centro de custo

---
+++ app/controllers/centro_de_custo_controller.rb
@@ -0,0 +1,20 @@
+class CentrosDeCustoController < ApplicationController
+  def index
+    render json: CentroDeCusto.all
+  end
+
+  def create
+    centro_de_custo = CentroDeCusto.new(centro_de_custo_params)
+    if centro_de_custo.save
+      render json: centro_de_custo, status: :created
+    else
+      render json: { errors: c.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def centro_de_custo_params
+    params.require(:centro_de_custo).permit(:nome)
+  end
+end
